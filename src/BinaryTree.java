a81b12da4738d87a06f06f874c215d89f368c904
Diddi <dps.dtt25@gmail.com>
2023-09-24 15:52:13 +0200

    inte rätt traverse

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -7,6 +7,39 @@ public class BinaryTree implements Iterable<Integer>{
     public Iterator<Integer> iterator() {
         return new TreeIterator();
     }
+    public void add(Integer key, Integer value) {
+        Node newNode = new Node(key, value);
+        Node nextNode = root;
+        if(nextNode == null) {
+            newNode = newNode;
+        }
+        if(nextNode.key.equals(key)) {
+            nextNode.value = newNode.value;
+        }
+    }
+    public boolean lookup(Integer key) {
+        Node nextNode = root;
+        while(nextNode != null) {
+            if(nextNode.key < key) {
+                nextNode = nextNode.left;
+                if(nextNode.key == key) {
+                    return true;
+                }
+            }
+            if(nextNode.key > key) {
+                nextNode = nextNode.right;
+                if(nextNode.key == key) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private Node traverse(Integer key) {
+        int pointer = 0;
+
+    }
 
 
 }
