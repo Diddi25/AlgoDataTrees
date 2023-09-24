2fde8f1512c7f984e092db955778adf2242a8cbe
Diddi <dps.dtt25@gmail.com>
2023-09-24 19:16:22 +0200

    traverse() och add() ser likadan ut

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -8,13 +8,28 @@ public class BinaryTree implements Iterable<Integer>{
         return new TreeIterator();
     }
     public void add(Integer key, Integer value) {
-        Node newNode = new Node(key, value);
         Node nextNode = this.root;
-        if(nextNode == null) {
-            this.root = newNode;
+        if (nextNode == null) { //om tree är empty()
+            this.root = new Node(key, value);
         }
-        Node foundedNode = traverse(key, nextNode);
-        foundedNode.right = nextNode;
+        if (nextNode.key == key) { //om nod har samma keyvärde
+            nextNode.value = value;
+            return;
+        }
+
+        if (nextNode.key < key) { //om nod just nu är till vänster
+            if(nextNode == null) {
+                nextNode = new Node(key, value);
+            } else {
+                nextNode = nextNode.left;
+            }
+        } else { //om nod just nu är till höger
+            if(nextNode == null) {
+                nextNode = new Node(key, value);
+            } else {
+                nextNode = nextNode.right;
+            }
+        }
     }
     public boolean lookup(Integer key) {
         Node nextNode = this.root;
@@ -27,14 +42,14 @@ public class BinaryTree implements Iterable<Integer>{
     }
 
     private Node traverse(Integer key, Node traverseNode) {
-        if(traverseNode.key < key) {
+        if (traverseNode.key < key) {
             if (traverseNode.left == null) {
                 return traverseNode;
             } else {
                 traverse(key, traverseNode.left);
             }
         } else {
-            if(traverseNode.right == null) {
+            if (traverseNode.right == null) {
                 return traverseNode;
             } else {
                 traverse(key, traverseNode.right);
