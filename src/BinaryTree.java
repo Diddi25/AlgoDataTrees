bcfeeff658bfae024156a451ee31e53ba915b63e
Diddi <diddi.dutta@outlook.com>
2023-09-25 13:27:05 +0200

    add() i Node{} funkar

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -8,19 +8,28 @@ public class BinaryTree implements Iterable<Integer>{
     public Iterator<Integer> iterator() {
         return new TreeIterator(this.root);
     }
-    public void add(Integer key, Integer value) {
+    public void add(Integer key, Integer value, int valuable) {
         Node nextNode = this.root;
         if (nextNode == null) { //om tree är empty()
             this.root = new Node(key, value);
             return;
         }
         Node leafParent = traverse(key, nextNode);
+        if(leafParent.key.equals(key)) {
+
+        }
         if(key < leafParent.key) {
             leafParent.left = new Node(key, value);
         } else {
             leafParent.right = new Node(key, value);
         }
-
+    }
+    public void add(Integer key, Integer value) {
+        if(this.root == null) {
+            this.root = new Node(key, value);
+        } else {
+            this.root.addi(key,value);
+        }
     }
     private Node traverse(Integer key, Node traverseNode) {
         if (traverseNode.key.equals(key)) { //om nod har samma keyvärde
@@ -69,6 +78,13 @@ public class BinaryTree implements Iterable<Integer>{
     }
 
     /*
+    @Override
+    public int compareTo(K o) {
+        return 0;
+    }
+    */
+
+    /*
     public void remove(Integer key) {
         Node nextNode = this.root;
         if(lookup(key)) {
@@ -91,6 +107,23 @@ public class BinaryTree implements Iterable<Integer>{
                 } else {
                     search(key, nextNode.right);
                 }
+            }
+        }
+    }
+    */
+    /*
+    public Node remove(Integer key) {
+        if(key == key) {
+            if(key > key) {
+                if(left != null) {
+                    left = left.delete(key);
+                }
+                return this;
+            } else {
+                if(right != null) {
+                    right = right.delete(key);
+                }
+                return this;
             }
         }
     }
--- src/Node.java
+++ src/Node.java
@@ -7,4 +7,24 @@ public class Node {
         this.value = value;
         this.left = this.right = null;
     }
+
+    public void addi(Integer key,Integer value) {
+        if (this.key.equals(key)) {
+            this.value = value ;
+            return;
+        }
+        if (this.key > key)
+            if (this.left != null)
+                this.left.addi(key, value);
+            else
+                this.left = new Node (key,value);
+        else {
+            if(this.right != null) {
+                this.right.addi(key, value);
+            } else {
+                this.right = new Node(key, value);
+            }
+        }
+    }
+
 }
